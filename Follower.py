8e10e7176b5abcc6dad805195faf9e9c8a0f8b9b
mnuria-conejero <98323672+mnuria-conejero@users.noreply.github.com>
2022-03-21 14:14:16 +0100

    Update Follower.py

--- Follower.py
+++ Follower.py
@@ -17,7 +17,7 @@ import message_filters
 import cv_bridge
 from cv_bridge import CvBridge
 from cv_bridge import CvBridgeError
-from geometry_msgs.msg  import PoseStamped,Pose,Twist
+from geometry_msgs.msg  import PoseStamped,Pose,Twist,TwistStamped
 import tf
 from nav_msgs.msg import Odometry
 
@@ -35,25 +35,6 @@ upper_limit_sun=(0.3402,0.9944,0.9969)
     #Cloudy day images
 lower_limit_cloud=(0.000268,0,5077,0.7160)
 upper_limit_cloud=(0.4216,0,9958,0.9978)
-
-#Difference between UWB and RealSense distance admitted (m)
-difference_threshold=1
-
-#Measurement of the robot (m)
-robot_measure=1.04
-    #Future threshold:1.54
-
-#Threshold in order to stop the robot when the operator is close (m)
-stop_threshold=1.64
-
-#Angular Velocity stablished for turning
-aV=0.25
-
-#Tolerance for the turning angle (rad-5 degrees)
-angle_tolerance=0.09
-
-#Proportional gain for velocity
-kp=0.4
 
 #-------------------------------------------------------------#FUNCTIONS-------------------------------------------------------------------------------------------------------------------------
 
@@ -79,7 +60,28 @@ def colorSegmentation(image,lower_limit,upper_limit):
     result=erode
     return result
 
-
+#Function to calculate Distance of the bounding box from the depth image
+#Inputs:
+    #-segmented_result: output from ColorSegmentation()
+    #-depth_array: image from camera converted to distance data
+    #-y1,y2,x1,x2: coordinates of the bounding box
+#Outputs:
+    #-distance_to_vest: distance in m to the detected vest
+def calculateDistance(segmented_result,depth_array,y1,y2,x1,x2):
+    #Get the mean distance from the pixels of the bounding box
+    mean_distance=0.0
+    mean_counter=0.0
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            if segmented_result[int(i),int(j)] == 255:
+                mean_distance=mean_distance+(depth_array[int(i),int(j)])/1000
+                mean_counter=mean_counter+1
+        
+    if mean_counter==0:
+        distance_to_bb=None
+    else:
+        distance_to_bb=mean_distance/mean_counter
+    return distance_to_bb
 
 #Function for obtaining the distance to the vest segmented in ColorSegmentation()
 #Inputs:
@@ -87,139 +89,99 @@ def colorSegmentation(image,lower_limit,upper_limit):
     #-depth_image: depth image from camera
 #Outputs:
     #-distance_to_vest: distance in m to the detected vest
-def getDepthDistance(segmented_result,depth_image,raw_depth):
+def getDepthDistance(segmented_result,depth_image,raw_depth,distance_to_anchor):
     #Extract boundind boxes from the segmented result by looking at the contours
     contours=cv2.findContours(segmented_result,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     contours=contours[0] if len(contours) == 2 else contours[1]
-    #Comparing all the contours to find out the biggest one
-    max=0
+    
+    min_difference=1000
     x,y,h,w=0,0,0,0
+    x_draw,y_draw,h_draw,w_draw=0,0,0,0
+
     for idx,cntr in enumerate(contours):
         xt,yt,wt,ht=cv2.boundingRect(cntr)
         xt=int(xt)
         yt=int(yt)
         wt=int(wt)
         ht=int(ht)
+
+        cv2.rectangle(segmented_result, (xt, yt), (xt+wt, yt+ht), (100, 0, 0), 2)
         size=wt*ht
-        if size>max:
-            max=size
+        if size>4000:
             x,y,w,h=xt,yt,wt,ht
-    
-    cv2.rectangle(segmented_result, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    cv2.imshow('Segmentation', segmented_result)
-    
-    if w*h > 4000: #If the BB area is > 4000 it is considered valid
-        x1=x
-        x2=x+w
-        y1=y
-        y2=y+h
+            x1=x
+            x2=x+w
+            y1=y
+            y2=y+h
 
-        #Check if the coordinates are lower or higher than dimensions
-        xmax=depth_image.shape[1]#width
-        ymax=depth_image.shape[0]#height
-        xmin=0
-        ymin=0
-        if x1<xmin:
-            x1=xmin
-        if x1>xmax:
-            x1=xmax
-        if y1<ymin:
-            y1=ymin
-        if y1>ymax:
-            y1=ymax
-        if x2<xmin:
-            x2=xmin
-        if x2>xmax:
-            x2=xmax
-        if y2<ymin:
-            y2=ymin
-        if y2>ymax:
-            y2=ymax
+            #Check if the coordinates are lower or higher than dimensions
+            xmax=depth_image.shape[1]#width
+            ymax=depth_image.shape[0]#height
+            xmin=0
+            ymin=0
+            if x1<xmin:
+                x1=xmin
+            if x1>xmax:
+                x1=xmax
+            if y1<ymin:
+                y1=ymin
+            if y1>ymax:
+                y1=ymax
+            if x2<xmin:
+                x2=xmin
+            if x2>xmax:
+                x2=xmax
+            if y2<ymin:
+                y2=ymin
+            if y2>ymax:
+                y2=ymax
         
         
-    
-        # Use cv_bridge() to convert the ROS image to OpenCV format
-        bridge = CvBridge()
-        try:
-            depth_image_array = bridge.imgmsg_to_cv2(raw_depth, desired_encoding="passthrough")
-        except CvBridgeError as e:
-            print(e)
+            # Use cv_bridge() to convert the ROS image to OpenCV format
+            bridge = CvBridge()
+            try:
+                depth_image_array = bridge.imgmsg_to_cv2(raw_depth, desired_encoding="passthrough")
+            except CvBridgeError as e:
+                print(e)
         
         
-        depth_array=np.array(depth_image_array, dtype=np.float32)
-        #Get the mean distance from the pixels of the bounding box
-        mean_distance=0.0
-        mean_counter=0.0
-        for i in range(y1,y2):
-            for j in range(x1,x2):
-                mean_distance=mean_distance+(depth_array[int(i),int(j)])/1000
-                mean_counter=mean_counter+1
+            depth_array=np.array(depth_image_array, dtype=np.float32)
+            distance_to_bb=calculateDistance(segmented_result,depth_array,y1,y2,x1,x2)
+
+            if distance_to_bb==None or distance_to_bb==0.0:
+                continue
+            elif abs(distance_to_anchor-distance_to_bb)<min_difference:
+                min_difference=distance_to_bb
+                x_draw,y_draw,w_draw,h_draw=x,y,w,h
+        else:
+            continue
+
         
-        distance_to_vest=mean_distance/mean_counter
-        return distance_to_vest
+
+    if min_difference==1000:
+        distance_to_vest=None
     else:
-        return None
+        distance_to_vest=min_difference
+        cv2.rectangle(segmented_result, (x_draw, y_draw), (x_draw+w_draw, y_draw+h_draw), (255, 0, 0), 2)
+        nameText=str(round(distance_to_vest,4)) + ' m'
+        cv2.putText(segmented_result, nameText, (x_draw, y_draw-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0), 2)
+        cv2.imshow('Segmentation', segmented_result)
+        
 
-
-#Function for getting the message in order to stop robot
-#Inputs:
-    #-None
-#Outputs:
-    #-Stop message: message to set all velocities to 0
-def stoppingRobot():
-    stop_message=Twist()
-    stop_message.linear.x=0.0
-    stop_message.linear.y=0.0
-    stop_message.linear.z=0.0
-    stop_message.angular.x=0.0
-    stop_message.angular.y=0.0
-    stop_message.angular.z=0.0
-    return stop_message
-
-
-#Function for obtaining angular velocity in order to make the turn needed
-#Inputs:
-    #-theta_sensor: operator orientation
-    #-theta_odom: orientation of the robot
-    #-aV: module of the angular velocity
-#Outputs:
-    #factor*aV: module and direction of the angular Velocity
-
-#-1: clockwise
-# 1: counterclockwise
-def angularVelocity(theta_sensor,theta_odom,aV):
-
-    if theta_odom>0:
-        if theta_sensor<=0:
-            factor=-1
-        elif theta_sensor>0:
-            if theta_odom>theta_sensor:
-                factor=-1
-            elif theta_odom<theta_sensor:
-                factor=1
-
-    elif theta_odom<0:
-        if theta_sensor>=0:
-            factor=1
-        elif theta_sensor<0:
-            if abs(theta_odom)>abs(theta_sensor):
-                factor=1
-            elif abs(theta_odom)<abs(theta_sensor):
-                factor=-1
-    elif theta_odom==0:
-        if theta_sensor==0:
-            factor=0
-        elif theta_sensor>0:
-            factor=1
-        elif theta_sensor<0:
-            factor=-1
+    return distance_to_vest
+        
     
-    return factor*aV
+
+
+
 
 
 #-------------------------------------------------------------------#MAIN----------------------------------------------------------------------------------------------------------------------
 
+
+
 def callback(color_data,depth_data,anchor_data,odom_data):
+    
     #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
     
     #Read images from topics
@@ -232,7 +194,7 @@ def callback(color_data,depth_data,anchor_data,odom_data):
     #Read odom data
     current_odometry = odom_data
     
-
+    
 
     #Segment Image and Get the distance to vest from camera
     image_segmented_all=colorSegmentation(im_color,lower_limit,upper_limit)
@@ -241,84 +203,39 @@ def callback(color_data,depth_data,anchor_data,odom_data):
     image_segmented1=cv2.add(image_segmented_all,image_segmented_sun)
     image_segmented=cv2.add(image_segmented1,image_segmented_cloud)
 
-    distance_to_operator=getDepthDistance(image_segmented,im_depth,depth_data)
 
     #Get anchor distance
     distance_to_anchor=anchorPose.pose.position.z
 
-    print(distance_to_operator,distance_to_anchor)
+    #Get distance to operator with camera
+    distance_to_operator=getDepthDistance(image_segmented,im_depth,depth_data,distance_to_anchor)
 
-    #We create the velocity publisher
-    nav_pub = rospy.Publisher("/robot/move_base/cmd_vel", Twist, queue_size=10)
-
-    #When no operator is detected by camera-STOP
-    if distance_to_operator==None:
-        print('NO OPERATOR DETECTED BY CAMERA-STOP')
-        #Stop movement
-        #Check movements in simulation
-        #stop_cmd=stoppingRobot()
-        #nav_pub.publish(stop_cmd)
-
-    #When operator detected by camera matches with UWB
-    elif abs(distance_to_operator-distance_to_anchor)<difference_threshold:
-        #If operator close - STOP
-        if distance_to_operator <= stop_threshold:
-            print('OPERATOR CLOSE-STOP')
-            #Stop movement
-            #Check movements in simulation
-            #stop_cmd=stoppingRobot()
-            #nav_pub.publish(stop_cmd)
-
-        #If operator not close - MOVE
-        else:
-            print('Sending Movement command-Moving robot to Pose')
-
-            
-            
-            #Get theta_odom from the robot Odometry
-            orientation = current_odometry.pose.pose.orientation
-            orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
-            euler = tf.transformations.euler_from_quaternion(orientation_list)
-            #When there is no velocity in y axis
-            theta_odom = euler[2]
-
-            #Get orientation from operator
-            theta_sensor= theta_odom + anchorPose.pose.orientation.z
-
-            print("Theta_sensor: ",theta_sensor," Theta_odom",theta_odom)
-
-            robotVel=Twist()
-
-            #Turn around z axis toward operator orientation
-            if (theta_odom < theta_sensor-angle_tolerance) or (theta_odom > theta_sensor+angle_tolerance):
-                robotVel.linear.x = 0.0
-                robotVel.angular.z = angularVelocity(theta_sensor,theta_odom,aV)
-
-                #Only for checking
-                if robotVel.angular.z>0:
-                    print("Turning left")
-                else:
-                    print("Turning right")
-            #When orientation is OK move forward with velocity increasing proportional to the distance
-            else:
-                robotVel.linear.x = kp*(distance_to_operator-robot_measure)
-                robotVel.angular.z = 0
-                print("Moving forward")
-
-
-
-            print(robotVel)
-
-            
-            #Check movements in simulation
-            #nav_pub.publish(robotVel)
     
-    #When operator detected by camera does not match with UWB-STOP
+
+
+    #Get theta_odom from the robot Odometry
+    orientation = current_odometry.pose.pose.orientation
+    orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+    euler = tf.transformations.euler_from_quaternion(orientation_list)
+    #When there is no velocity in y axis
+    theta_odom = euler[2]
+
+
+    #Get orientation from operator
+    raw_pose= anchorPose.pose.orientation.z
+    theta_anchor= theta_odom + raw_pose
+
+
+    #Update parameters
+    rospy.set_param('/distance_to_anchor', float(distance_to_anchor))
+    if distance_to_operator==None:
+        rospy.set_param('/distance_to_operator', False)
     else:
-        print('OPERATOR DETECTED BY CAMERA IS NOT OKAY-STOP')
-        #Stop movement
-        #stop_cmd=stoppingRobot()
-        #nav_pub.publish(stop_cmd)
+        rospy.set_param('/distance_to_operator', float(distance_to_operator))
+    rospy.set_param('/theta_odom', float(theta_odom))
+    rospy.set_param('/theta_anchor', float(theta_anchor))
+
+
 
     #waits for user to press any key 
     #(this is necessary to avoid Python kernel form crashing)
@@ -328,7 +245,9 @@ def callback(color_data,depth_data,anchor_data,odom_data):
     #cv2.destroyAllWindows() 
 
 
+
 #---------------------------------------------------------#TOPICS RECEIVED-------------------------------------------------------------------------------------------------------------------------------
+
 
 def listener():
 
@@ -337,7 +256,7 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('Image_and_distance_reader', anonymous=True)
+    rospy.init_node('Image_and_distance_reader', anonymous=False)
     
   
     im_color_subs=message_filters.Subscriber('/camera/color/image_raw', Image)
@@ -346,12 +265,22 @@ def listener():
     odom=message_filters.Subscriber('/robot/robotnik_base_control/odom', Odometry)
     
 
-    ts = message_filters.ApproximateTimeSynchronizer([im_color_subs,im_depth_subs,anchor_orientation,odom],10,0.1,allow_headerless=False)
+    ts = message_filters.ApproximateTimeSynchronizer([im_color_subs,im_depth_subs,anchor_orientation,odom],10,0.1,allow_headerless=True)
     ts.registerCallback(callback)
 
+   
+
     # spin() simply keeps python from exiting until this node is stopped
+    
     rospy.spin()
 
 if __name__ == '__main__':
+    rospy.set_param('/distance_to_anchor', float(0.0))
+    rospy.set_param('/distance_to_operator', float(0.0))
+    rospy.set_param('/theta_odom', float(0.0))
+    rospy.set_param('/theta_anchor', float(0.0))
+
     listener()
+   
+    
 
